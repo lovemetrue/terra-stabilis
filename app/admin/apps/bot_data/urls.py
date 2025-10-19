6ba2a164463776fa73ea0ff706ff20f7507379b5
Dmitrii <legkodogatca@gmail.com>
2025-10-19 09:59:35 +0300

    terra project

--- app/admin/apps/bot_data/urls.py
+++ app/admin/apps/bot_data/urls.py
@@ -6,7 +6,6 @@ app_name = 'bot_data'
 
 urlpatterns = [
 
-    path('dashboard/', views.bot_dashboard, name='bot_dashboard'),
     path('leads/', views.bot_leads, name='bot_leads'),
     path('events/', views.bot_events, name='bot_events'),
     path('calculations/', views.bot_calculations, name='bot_calculations'),
